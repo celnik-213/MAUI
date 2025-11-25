035984ecbe4270092fe61f29be616d5454b8f253
celnik-213 <uT_4000@zsp3zamosc.pl>
2025-11-25 13:23:19 +0100

    Diabelskie zadanie

---
+++ Zadanie_25_11/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Zadanie_25_11
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
